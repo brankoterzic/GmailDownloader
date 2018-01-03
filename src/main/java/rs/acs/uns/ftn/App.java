6da20c1c7c6f39cfdcbb6820c75a953c08a7657a
Branko Terzic <brankoterzi91@gmail.com>
2018-01-03 18:03:08 +0100

    clean up

--- src/main/java/rs/acs/uns/ftn/App.java
+++ src/main/java/rs/acs/uns/ftn/App.java
@@ -2,6 +2,8 @@ package rs.acs.uns.ftn;
 
 import java.io.File;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class App {
 
@@ -10,23 +12,23 @@ public class App {
      * @throws ParseException 
      */	
     public static void main(String[] args) throws ParseException {
-//        String host = "pop.gmail.com";
-//        String port = "995";
-//        String userName = "terza91.zl@gmail.com";
-//        String password = "alinpotok123";
-//        Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse("2017-01-01");
-//        Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2017-01-31");
-        String saveDirectory = "C:\\Users\\brankoterzic\\Desktop\\testCB";
-//
-//        EmailReader receiver = new EmailReader();
-//        receiver.setSaveDirectory(saveDirectory);
-//        receiver.downloadEmailAttachments(host, port, userName, password, startDate, endDate);
-//        
-//        Unzipper unz = new Unzipper();
-//        unz.unzipZip(saveDirectory);
-//
-//        ProjectCleaner cl = new ProjectCleaner();
-//        cl.cleanProjects(saveDirectory);
+        String host = "pop.gmail.com";
+        String port = "995";
+        String userName = "baze1.projekti@gmail.com";
+        String password = "";
+        Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse("2017-01-01");
+        Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2017-01-31");
+        String saveDirectory = ""; // add a directory path of directory where mail attachments should be saved
+
+        EmailReader receiver = new EmailReader();
+        receiver.setSaveDirectory(saveDirectory);
+        receiver.downloadEmailAttachments(host, port, userName, password, startDate, endDate);
+        
+        Unzipper unz = new Unzipper();
+        unz.unzipZip(saveDirectory);
+
+        ProjectCleaner cl = new ProjectCleaner();
+        cl.cleanProjects(saveDirectory);
     	
     	ProjectCleanerRecursive pcr = new ProjectCleanerRecursive();
     	pcr.extractFiles(new File(saveDirectory));
